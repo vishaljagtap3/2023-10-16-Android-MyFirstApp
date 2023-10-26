34a43b6bb7e9952b9210b53ff139606ce3e262b3
vishal <vishal.jagtap@bitcode.in>
2023-10-26 10:50:44 +0530

    Second version of the app

--- app/src/main/java/com/bitcodetech/myapp/MainActivity.java
+++ app/src/main/java/com/bitcodetech/myapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.bitcodetech.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -62,17 +64,67 @@ public class MainActivity extends Activity {
         btnSetMessage.setText("Set Message");
         mainContainer.addView(btnSetMessage);
 
-        btnSetMessage.setOnClickListener(new BtnSetMessageClickListener());
-        txtWelcomeMessage.setOnClickListener(new TxtWelcomeMessageClickListener());
+        //way 1 of listeners
+       /* btnSetMessage.setOnClickListener(new BtnSetMessageClickListener());
+        btnSetMessage.setOnTouchListener(new BtnSetMessageTouchListener());
+        txtWelcomeMessage.setOnClickListener(new TxtWelcomeMessageClickListener());*/
+
+        //way 2 of listeners
+        /*View.OnClickListener onClickListener = new MyViewClickListener();
+        btnSetMessage.setOnClickListener(onClickListener);
+        txtWelcomeMessage.setOnClickListener(onClickListener);*/
+
+        btnSetMessage.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        txtWelcomeMessage.setText(edtMessage.getText().toString());
+                    }
+                }
+        );
+
+        txtWelcomeMessage.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        txtWelcomeMessage.setText("");
+                    }
+                }
+        );
 
         setContentView(mainContainer);
 
+    }
+
+    private class MyViewClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(view instanceof Button) {
+                txtWelcomeMessage.setText(edtMessage.getText().toString());
+            }
+            else {
+                txtWelcomeMessage.setText("");
+            }
+        }
     }
 
     private class TxtWelcomeMessageClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             txtWelcomeMessage.setText("");
+        }
+    }
+
+    private class BtnSetMessageTouchListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                Log.e("tag", "touch down");
+            }
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                Log.e("tag", "touch up");
+            }
+            return false;
         }
     }
 
